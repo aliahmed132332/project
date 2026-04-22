1e48935c0959554a32ffd12963cac75570a65db5
aliahmed132332 <alih132332@gmail.com>
2026-04-22 17:05:15 +0300

    Add a simple console application for name input

---
+++ Ali AHMED.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("اكتب اسمك: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("اسمك هو: " + name);
+    }
+}
